99773e87b4267e3548549d0d037656cce8c754da
kidig <kidig@yandex-team.ru>
2022-03-25 19:26:04 +0300

    fix presigned urls for custom endpoint

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
-import pytest
+from urllib.parse import urlparse
 
-from s3upload.utils import get_s3_path_from_url, get_bucket_endpoint_url
+import pytest
+from django.test import override_settings
+
+from s3upload.utils import get_bucket_endpoint_url, get_s3_path_from_url
 
 TEST_BUCKET = "test-bucket-name"
 TEST_KEY = "folder1/folder2/file1.json"
@@ -26,13 +29,35 @@ def test_get_s3_path_from_url(url: str) -> None:
     "url,expected",
     [
         (None, f"https://{TEST_BUCKET}.s3.{TEST_REGION}.amazonaws.com"),
-        ("https://{bucket}.s3.{region}.amazonaws.com", f"https://{TEST_BUCKET}.s3.{TEST_REGION}.amazonaws.com"),
-        ("https://s3.{region}.amazonaws.com", f"https://s3.{TEST_REGION}.amazonaws.com"),
-        ("https://{bucket}.s3.amazonaws.com", f"https://{TEST_BUCKET}.s3.amazonaws.com"),
-    ]
+        (
+            "https://{bucket}.s3.{region}.amazonaws.com",
+            f"https://{TEST_BUCKET}.s3.{TEST_REGION}.amazonaws.com",
+        ),
+        (
+            "https://s3.{region}.amazonaws.com",
+            f"https://s3.{TEST_REGION}.amazonaws.com",
+        ),
+        (
+            "https://{bucket}.s3.amazonaws.com",
+            f"https://{TEST_BUCKET}.s3.amazonaws.com",
+        ),
+    ],
 )
 def test_get_bucket_endpoint_url(url: str, expected: str) -> None:
     kwargs = {}
     if url:
         kwargs["default"] = url
-    assert get_bucket_endpoint_url(bucket_name=TEST_BUCKET, region=TEST_REGION, **kwargs) == expected
+    assert (
+        get_bucket_endpoint_url(bucket_name=TEST_BUCKET, region=TEST_REGION, **kwargs)
+        == expected
+    )
+
+
+@override_settings(
+    S3UPLOAD_REGION=TEST_REGION,
+)
+def test_get_signed_download_url() -> None:
+    presigned_url = get_signed_download_url(key=TEST_KEY, bucket_name=TEST_BUCKET)
+    parsed_url = urlparse(presigned_url)
+    assert parsed_url.scheme == "https"
+    assert parsed_url.netloc == f"{TEST_BUCKET}.s3.{TEST_REGION}.amazonaws.com"
